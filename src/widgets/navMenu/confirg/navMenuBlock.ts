9fba7602310df92ca0428f7231d833fb801f1758
Максим <akaikinmax@mail.ru>
2024-02-25 04:49:23 +0700

    feat / реализация навигационного меню

---
+++ src/widgets/navMenu/confirg/navMenuBlock.ts
@@ -0,0 +1,23 @@
+interface MenuItem {
+  text: string;
+  path: string;
+}
+
+export const links: MenuItem[] = [
+  {
+    text: 'Home',
+    path: '/'
+  },
+  {
+    text: 'About',
+    path: '/'
+  },
+  {
+    text: 'Sevices',
+    path: '/'
+  },
+  {
+    text: 'Projects',
+    path: '/'
+  }
+];
---
+++ src/widgets/navMenu/ui/NavMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="nav-menu" :class="{ active: props.isActive }">
+    <ul class="nav-menu__list">
+      <li v-for="link in links" v-bind:key="link.path" class="nav-menu__item">
+        <a :href="link.path">{{ link.text }}</a>
+      </li>
+    </ul>
+    <div class="nav-menu__lang-btn">
+      <button>Lang</button>
+    </div>
+    <div class="nav-menu__message-btn">
+      <MyButton text="fdsf" :isLink="true"/>
+    </div>
+    <div class="nav-menu__social-btn">
+      <button>social btn</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import './style.scss';
+import { defineProps } from 'vue';
+import { links } from '../confirg/navMenuBlock';
+import { MyButton } from '@shared/ui';
+
+const props = defineProps<{
+  isActive: boolean
+}>();
+</script>
