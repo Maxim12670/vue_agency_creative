9768946fd21d2ba7e8929968fc66a64279706b7d
Максим <akaikinmax@mail.ru>
2024-02-25 04:49:47 +0700

    feat / реализация шапки

---
+++ src/widgets/mainHeader/ui/MainHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <content-container>
+    <header class="header">
+      <router-link to="/">
+        <img class="header__logo" src="../../../shared/assets/icons/logo.svg" alt="logo">
+      </router-link>
+      <NavMenu class="header__nav-menu" :isActive="isActive"/>
+      <div class="header__burger" @click="isActive = !isActive">
+        <div class="burger" :class="{active : isActive}">
+          <span class="burger__line"></span>
+        </div>
+      </div>
+    </header>
+  </content-container>
+</template>
+
+<script setup lang="ts">
+import './style.scss';
+import { ContentContainer} from '@shared/ui';
+import { NavMenu } from '@widgets/navMenu/index';
+import { ref } from 'vue';
+
+const isActive = ref<boolean>(false);
+</script>
