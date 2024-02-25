553d4ef2d95fa5f11d29615b5a55da50309b2c8e
Максим <akaikinmax@mail.ru>
2024-02-25 16:59:33 +0700

    feat / реализована кнопка перелючения языков

---
+++ src/widgets/buttonLang/ui/ButtonLang.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="lang">
+    <input class="lang__input" v-model="selectedLanguage" type="radio" name="lang" value="en" id="en">
+    <label class="lang__label" for="en">EN</label>
+    <input class="lang__input" v-model="selectedLanguage" type="radio" name="lang" value="ru" id="ru">
+    <label class="lang__label" for="ru">RU</label>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import './style.scss'
+import { ref, watch } from 'vue'
+// import i18n from '@app/boot/tanStack'
+
+const selectedLanguage = ref('en');
+
+watch(selectedLanguage, (newValue) => {
+  // i18n.locale = newValue;
+});
+</script>
