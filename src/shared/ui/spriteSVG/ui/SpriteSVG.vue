ba35a14d2a9c7d80504646500f6b06cd997b82aa
Максим <akaikinmax@mail.ru>
2024-02-26 23:08:26 +0700

    feat / svg спрайт для svg картинок

---
+++ src/shared/ui/spriteSVG/ui/SpriteSVG.vue
@@ -0,0 +1,55 @@
+<template>
+  <svg style="display: none">
+    <symbol id="dotters64" viewBox="0 0 98.4766 70.9255">
+      <g>
+        <ellipse id="Ellipse 58" cx="2.500000" cy="2.624756" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 70" cx="2.500000" cy="46.409180" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 60" cx="39.890625" cy="2.624756" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 71" cx="39.890625" cy="46.409180" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 62" cx="77.281250" cy="2.624756" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 72" cx="77.281250" cy="46.409180" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 64" cx="2.500000" cy="24.516846" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 73" cx="2.500000" cy="68.301025" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 66" cx="39.890625" cy="24.516846" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 74" cx="39.890625" cy="68.301025" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 68" cx="77.281250" cy="24.516846" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 75" cx="77.281250" cy="68.301025" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 59" cx="21.195312" cy="2.624756" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 76" cx="21.195312" cy="46.409180" rx="2.500000" ry="2.624691"
+          fill-opacity="1.000000" />
+        <ellipse id="Ellipse 61" cx="58.585938" cy="2.624756" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+      <ellipse id="Ellipse 77" cx="58.585938" cy="46.409180" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+      <ellipse id="Ellipse 63" cx="95.976562" cy="2.624756" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+      <ellipse id="Ellipse 78" cx="95.976562" cy="46.409180" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+      <ellipse id="Ellipse 65" cx="21.195312" cy="24.516846" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+      <ellipse id="Ellipse 79" cx="21.195312" cy="68.301025" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+      <ellipse id="Ellipse 67" cx="58.585938" cy="24.516846" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+      <ellipse id="Ellipse 80" cx="58.585938" cy="68.301025" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+      <ellipse id="Ellipse 69" cx="95.976562" cy="24.516846" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+      <ellipse id="Ellipse 81" cx="95.976562" cy="68.301025" rx="2.500000" ry="2.624691"
+        fill-opacity="1.000000" />
+    </g>
+  </symbol>
+</svg></template>
