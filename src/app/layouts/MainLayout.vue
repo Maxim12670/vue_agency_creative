5dd9384d7be3f5a769bd8dd6b62d33402e7de3ce
Максим <akaikinmax@mail.ru>
2024-02-27 11:47:54 +0700

    feat / реализация плавного появления при скролле

--- src/app/layouts/MainLayout.vue
+++ src/app/layouts/MainLayout.vue
@@ -2,11 +2,42 @@
   <q-layout view="lHh Lpr lFf">
     <MainHeaderComponent />
     <q-page-container>
-      <!-- тут будут секции странички -->
+      <div>
+        <HelpForBusiness />
+      </div>
+      <div class="section-animation">
+        <AboutUs />
+      </div>
+
     </q-page-container>
   </q-layout>
 </template>
 <script setup lang="ts">
+import './style.scss';
 import { MainHeaderComponent } from '@widgets/mainHeader';
+import { HelpForBusiness } from '@widgets/helpForBusiness';
+import { AboutUs } from '@widgets/aboutUs';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const onEntry = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  };
+
+  const options: IntersectionObserverInit = {
+    threshold: [0.4]
+  };
+
+  const observer = new IntersectionObserver(onEntry, options);
+  const elements = document.querySelectorAll<HTMLElement>('.section-animation');
+  console.log(elements.length)
+
+  elements.forEach(element => {
+    observer.observe(element);
+  });
+});
 </script>
 
