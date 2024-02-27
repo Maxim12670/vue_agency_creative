09f595936321813fde96d46dd1185220e3d8649b
Максим <akaikinmax@mail.ru>
2024-02-27 11:50:43 +0700

    fix / изменил логику реализации

--- src/widgets/buttonLang/ui/ButtonLang.vue
+++ src/widgets/buttonLang/ui/ButtonLang.vue
@@ -1,30 +1,15 @@
 <template>
   <div class="lang">
-    <div v-for="item in languages" :key="item.lang">
-      <input class="lang__input" v-model="selectedLanguage" type="radio" name="lang" :value="item.lang" :id="item.lang">
-      <label class="lang__label" :for="item.lang">{{ item.lang }}</label>
+    <div v-for="item in availableLocales" :key="item">
+      <input class="lang__input" v-model="locale" type="radio" name="lang" :value="item" :id="item">
+      <label class="lang__label" :for="item">{{ item }}</label>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import './style.scss'
-import { ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-const languages = [
-  {
-    lang: 'en'
-  },
-  {
-    lang: 'ru'
-  }
-]
-
-const { locale } = useI18n({ useScope: 'global' });
-const selectedLanguage = ref(locale);
-
-watch(selectedLanguage, (newValue) => {
-  $i18n.global.locale = newValue;
-});
+const { locale, availableLocales } = useI18n();
 </script>
