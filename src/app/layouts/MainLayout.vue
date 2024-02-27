6587a14442a78e78d81c17cb5f638a193a74b1a1
Максим <akaikinmax@mail.ru>
2024-02-27 14:18:54 +0700

    feat / подключение секции services

--- src/app/layouts/MainLayout.vue
+++ src/app/layouts/MainLayout.vue
@@ -5,10 +5,12 @@
       <div>
         <HelpForBusiness />
       </div>
-      <div class="section-animation">
+      <div class="section-animation show">
         <AboutUs />
       </div>
-
+      <div class="section-animation show">
+        <OurServices/>
+      </div>
     </q-page-container>
   </q-layout>
 </template>
@@ -17,27 +19,28 @@ import './style.scss';
 import { MainHeaderComponent } from '@widgets/mainHeader';
 import { HelpForBusiness } from '@widgets/helpForBusiness';
 import { AboutUs } from '@widgets/aboutUs';
+import { OurServices } from '@widgets/ourServices';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const onEntry = (entries: IntersectionObserverEntry[]) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      }
-    });
-  };
+// document.addEventListener('DOMContentLoaded', () => {
+//   const onEntry = (entries: IntersectionObserverEntry[]) => {
+//     entries.forEach(entry => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add('show');
+//       }
+//     });
+//   };
 
-  const options: IntersectionObserverInit = {
-    threshold: [0.4]
-  };
+//   const options: IntersectionObserverInit = {
+//     threshold: [0.4]
+//   };
 
-  const observer = new IntersectionObserver(onEntry, options);
-  const elements = document.querySelectorAll<HTMLElement>('.section-animation');
-  console.log(elements.length)
+//   const observer = new IntersectionObserver(onEntry, options);
+//   const elements = document.querySelectorAll<HTMLElement>('.section-animation');
+//   console.log(elements.length)
 
-  elements.forEach(element => {
-    observer.observe(element);
-  });
-});
+//   elements.forEach(element => {
+//     observer.observe(element);
+//   });
+// });
 </script>
 
