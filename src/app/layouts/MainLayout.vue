595a949002269736699b6f70eaccfa8c6b73a5d2
Максим <akaikinmax@mail.ru>
2024-02-28 13:49:30 +0700

    feat / добавил секцию partfolio

--- src/app/layouts/MainLayout.vue
+++ src/app/layouts/MainLayout.vue
@@ -2,6 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <MainHeaderComponent />
     <q-page-container>
+      <modalWindow/>
       <div>
         <HelpForBusiness />
       </div>
@@ -11,15 +12,31 @@
       <div class="section-animation show">
         <OurServices/>
       </div>
+      <div class="section-animation show">
+        <OurPortfolio/>
+      </div>
     </q-page-container>
   </q-layout>
 </template>
 <script setup lang="ts">
 import './style.scss';
+import {ref, provide} from 'vue';
 import { MainHeaderComponent } from '@widgets/mainHeader';
 import { HelpForBusiness } from '@widgets/helpForBusiness';
 import { AboutUs } from '@widgets/aboutUs';
 import { OurServices } from '@widgets/ourServices';
+import { OurPortfolio } from '@widgets/ourPortfolio';
+import { ModalWindow } from '@widgets/modalWindow';
+
+const isModal = ref<boolean>(false);
+const updateModalValue = () => {
+  isModal.value = !isModal.value;
+}
+
+provide('modal', {
+  isModal,
+  updateModalValue
+})
 
 document.addEventListener('DOMContentLoaded', () => {
   const onEntry = (entries: IntersectionObserverEntry[]) => {
