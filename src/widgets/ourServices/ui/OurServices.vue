dfd1140d4a6f4262485c6b8dddf6197fc2531b4a
Максим <akaikinmax@mail.ru>
2024-02-28 13:47:15 +0700

    feat / изменил структуру вертски

--- src/widgets/ourServices/ui/OurServices.vue
+++ src/widgets/ourServices/ui/OurServices.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="services">
+    <SpriteSVG />
     <content-container>
       <div class="services__wrapp">
         <div class="services__container">
@@ -19,7 +20,9 @@
 
         <div class="services__list">
           <div class="card" v-for="card in cards" :key="card.textLocaleKey">
-            <img class="card__icon" :src="card.srcImg" alt="logo">
+            <svg class="card__icon">
+              <use :xlink:href="`#${card.id}`"></use>
+            </svg>
             <h2 class="card__title">
               {{ $t(card.textLocaleKey) }}
             </h2>
@@ -31,26 +34,44 @@
 </template>
 
 <script setup lang="ts">
-import { ContentContainer, MyButton } from '@shared/ui';
+import { ContentContainer, MyButton, SpriteSVG } from '@shared/ui';
 import './style.scss';
-
 
 const cards = [
   {
     textLocaleKey: 'services.cards.social',
-    srcImg: '@shared/assets/icons/ourServices/media-icon.svg'
+    id: 'media-icon'
   },
   {
     textLocaleKey: 'services.cards.optimization',
-    srcImg: '@shared/assets/icons/ourServices/optimization-icon.svg'
+    id: 'optimization-icon'
   },
   {
     textLocaleKey: 'services.cards.design',
-    srcImg: '@shared/assets/icons/ourServices/design-icon.svg'
+    id: 'design-icon'
   },
   {
     textLocaleKey: 'services.cards.ads',
-    srcImg: '@shared/assets/icons/ourServices/ads-icon.svg'
+    id: 'ads-icon'
   },
 ]
+
+// const cards = [
+//   {
+//     textLocaleKey: 'services.cards.social',
+//     srcImg: '@shared/assets/icons/ourServices/media-icon.svg'
+//   },
+//   {
+//     textLocaleKey: 'services.cards.optimization',
+//     srcImg: '@shared/assets/icons/ourServices/optimization-icon.svg'
+//   },
+//   {
+//     textLocaleKey: 'services.cards.design',
+//     srcImg: '@shared/assets/icons/ourServices/design-icon.svg'
+//   },
+//   {
+//     textLocaleKey: 'services.cards.ads',
+//     srcImg: '@shared/assets/icons/ourServices/ads-icon.svg'
+//   },
+// ]
 </script>
